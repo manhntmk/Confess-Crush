1037c6e371b4d4a81e8203dce3de6786acfd6db3
manhntmk <89734967+manhntmk@users.noreply.github.com>
2021-09-07 14:38:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Gửi em",
+    introTitle: 'Vợ của lươn lẹo',
+    introDesc: `Nếu như có một người luôn chờ, yêu, hy vọng em sẽ cùng chung bước với người đó trên một con đường... và người đó đang nói với em điều này, em sẽ nghĩ gì?`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -14,7 +11,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Nào gặp anh ăn em nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
